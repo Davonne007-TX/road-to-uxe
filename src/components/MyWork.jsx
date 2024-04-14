eb75b8d921dd090d8a760509fb011c12ff163f72
Vonney <davonnejv@gmail.com>
2024-04-14 15:45:47 -0500

    fixed MyWork.jsx, mapped over my work so its not so much code

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -1,38 +1,24 @@
 import { Link } from "react-router-dom";
 
 export default function MyWork() {
+  const myWork = [
+    { id: 1, text: "St. Patrick's Day", link: "/st-patricks-day" },
+    { id: 2, text: "Tobacco Shop", link: "/linkedIn" },
+    { id: 3, text: "LinkedIn Mobile Login", link: "/next" },
+    { id: 4, text: "240sx Nissan", link: "/blackPanther" },
+    { id: 5, text: "Peace and Love", link: "chasing" },
+    { id: 6, text: "Storm Chasers", link: "/login" },
+    { id: 7, text: "Easter", link: "/search" },
+    { id: 8, text: "Coca Cola", link: "/cola" },
+  ];
+
   return (
-    <nav className="grid grid-cols-1 md:grid-cols-2 text-teal lg:grid-cols-3 lg:flex-row gap-10 lg:gap-8 font-sans text-2xl p-8">
-      <Link to="/st-patricks-day" className="hover:underline text-teal">
-        St.Patrick's Day
-      </Link>
-      <Link to="/shop" className="hover:underline text-teal">
-        Tobacco Shop
-      </Link>
-      <Link to="/linkedIn" className="hover:underline text-teal">
-        LinkedIn Mobile Login
-      </Link>
-      <Link to="/blackPanther" className="hover:underline text-teal">
-        240sx Nissan
-      </Link>
-      <Link to="/like" className="hover:underline text-teal">
-        Peace and Love
-      </Link>
-      <Link to="/chasing" className="hover:underline text-teal">
-        Storm Chasers
-      </Link>
-      <Link to="/login" className="hover:underline text-teal">
-        X Login
-      </Link>
-      <Link to="/search" className="hover:underline text-teal">
-        Search
-      </Link>
-      <Link to="/easter" className="hover:underline text-teal">
-        Easter
-      </Link>
-      <Link to="/cola" className="hover:underline text-teal">
-        Coca Cola
-      </Link>
+    <nav className="grid grid-cols-1 md:grid-cols-2 mt-10 text-teal lg:grid-cols-3 lg:flex-row gap-10 lg:gap-8 font-sans text-2xl">
+      {myWork.map((work, id) => (
+        <li key="id" className="text-teal list-none">
+          <Link to={id.link}>{work.text}</Link>
+        </li>
+      ))}
     </nav>
   );
 }
