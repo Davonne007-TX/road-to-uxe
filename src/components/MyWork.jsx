a15f49466fe02330dadae93b38ce7da56fea853f
Vonney <davonnejv@gmail.com>
2024-04-15 12:55:16 -0500

    updated to links to projects on MyWork.jsx

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -3,20 +3,20 @@ import { Link } from "react-router-dom";
 export default function MyWork() {
   const myWork = [
     { id: 1, text: "St. Patrick's Day", link: "/st-patricks-day" },
-    { id: 2, text: "Tobacco Shop", link: "/linkedIn" },
+    { id: 2, text: "Tobacco Shop", link: "/shop" },
     { id: 3, text: "LinkedIn Mobile Login", link: "/next" },
     { id: 4, text: "240sx Nissan", link: "/blackPanther" },
-    { id: 5, text: "Peace and Love", link: "chasing" },
-    { id: 6, text: "Storm Chasers", link: "/login" },
+    { id: 5, text: "Peace and Love", link: "/like" },
+    { id: 6, text: "Storm Chasers", link: "/chasing" },
     { id: 7, text: "Easter", link: "/search" },
     { id: 8, text: "Coca Cola", link: "/cola" },
   ];
 
   return (
-    <nav className="grid grid-cols-1 md:grid-cols-2 mt-10 text-teal lg:grid-cols-3 lg:flex-row gap-10 lg:gap-8 font-sans text-2xl">
+    <nav className="grid grid-cols-1 md:grid-cols-2 mt-4 text-teal lg:grid-cols-3 lg:flex-row gap-10 lg:gap-8 font-sans text-2xl">
       {myWork.map((work, id) => (
-        <li key="id" className="text-teal list-none">
-          <Link to={id.link}>{work.text}</Link>
+        <li key={id} className="text-teal list-none">
+          <Link to={work.link}>{work.text}</Link>
         </li>
       ))}
     </nav>
