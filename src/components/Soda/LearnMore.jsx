805322409f3caf441881d31d5d811add08f380ca
Vonney <davonnejv@gmail.com>
2024-05-07 19:54:53 -0500

    adding styles to LearnMore.jsx

--- src/components/Soda/LearnMore.jsx
+++ src/components/Soda/LearnMore.jsx
@@ -1,6 +1,6 @@
 export default function LearnMore() {
   return (
-    <button className="text-black text-lg">
+    <button className="text-black text-lg mr-auto">
       <a href="">Learn More &#8594; </a>
     </button>
   );
