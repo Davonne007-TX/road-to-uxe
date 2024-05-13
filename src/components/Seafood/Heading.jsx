5cc7b3ec649444baeb9d6466bd75f16011504065
Vonney <davonnejv@gmail.com>
2024-05-13 17:43:34 -0500

    feat: adding nav bar

--- src/components/Seafood/Heading.jsx
+++ src/components/Seafood/Heading.jsx
@@ -1,7 +1,18 @@
 export default function Heading() {
   return (
-    <h1 className="text-8xl text-white ml-20 absolute mt-10 font-rak">
-      Prongs
-    </h1>
+    <>
+      <header className="flex flex-row justify-between items-center gap-40 absolute mt-10 text-white">
+        <h1 className="text-8xl ml-20 font-rak">Prongs</h1>
+
+        <nav>
+          <ul className="flex flex-row justify-between items-center text-2xl mt-10 gap-20">
+            <li>Menu</li>
+            <li>About</li>
+            <li>Locations</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
