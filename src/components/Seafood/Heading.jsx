8ffc4dbfb1b04509d48f3bbf7e32fc381641312c
Vonney <davonnejv@gmail.com>
2024-05-13 17:53:05 -0500

    updated nav bar to be mapped over

--- src/components/Seafood/Heading.jsx
+++ src/components/Seafood/Heading.jsx
@@ -1,15 +1,20 @@
 export default function Heading() {
+  const links = [
+    { name: "Menu" },
+    { name: "About" },
+    { name: "Locations" },
+    { name: "Contact" },
+  ];
   return (
     <>
       <header className="flex flex-row justify-between items-center gap-40 absolute mt-10 text-white">
         <h1 className="text-8xl ml-20 font-rak">Prongs</h1>
 
         <nav>
-          <ul className="flex flex-row justify-between items-center text-2xl mt-10 gap-20">
-            <li>Menu</li>
-            <li>About</li>
-            <li>Locations</li>
-            <li>Contact</li>
+          <ul className="flex flex-row items-center text-2xl mt-10 gap-20">
+            {links.map((link, index) => (
+              <li key={index}>{link.name}</li>
+            ))}
           </ul>
         </nav>
       </header>
