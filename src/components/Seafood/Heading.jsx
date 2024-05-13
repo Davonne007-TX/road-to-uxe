0d68f6dc4748ce96da2515514167450facd0f27b
Vonney <davonnejv@gmail.com>
2024-05-13 16:33:54 -0500

    feat: title heading

---
+++ src/components/Seafood/Heading.jsx
@@ -0,0 +1,7 @@
+export default function Heading() {
+  return (
+    <h1 className="text-8xl text-white ml-20 absolute mt-10 font-rak">
+      Prongs
+    </h1>
+  );
+}
--- src/components/Seafood/Shrimp.jsx
+++ src/components/Seafood/Shrimp.jsx
@@ -1,10 +1,14 @@
+import Heading from "./Heading";
+
 export default function Shrimp() {
   return (
     <main className="bg-[url('/images/prongs.jpg')] bg-cover h-screen">
+      <Heading />
+
       <section className="flex flex-col justify-center items-center h-screen text-white gap-4">
-        <h1 className="text-5xl md:text-7xl lg:text-8xl font-rak">
+        <h2 className="text-5xl md:text-7xl lg:text-8xl font-rak">
           All You Can Eat
-        </h1>
+        </h2>
         <p className="text-md md:text-2xl lg:text-4xl font-serif">
           From shrimp, lobster, to scallops, all you can eat seafood for $11.99!
         </p>
