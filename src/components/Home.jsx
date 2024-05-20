9c2496e9a3dec382c519f428c03ea0576a44653b
Vonney <davonnejv@gmail.com>
2024-05-20 11:53:52 -0500

    updating UI for Home.jsx and updated link to LinkedIn project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,43 +3,15 @@ import MyWork from "./MyWork";
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col justify-center items-center min-h-screen bg-peach">
-        <section className="flex flex-col justify-center items-center mt-20 mb-20 max-w-3xl lg:max-w-7xl rounded-2xl p-8 bg-neutral-100">
-          <h1 className=" text-5xl lg:text-6xl font-rak flex flex-col text-teal justify-center items-center">
-            Davonne J. Vigil
-          </h1>
-          <section className="flex flex-col justify-center items-center m-auto max-w-4xl  gap-8 text-balance  text-xl mt-8 font-serif p-4 text-teal">
-            <p>
-              Time to bridge the gap between design and development. With this
-              repository, I am focusing on building user interfaces from the
-              design perspective. I am also developing my React.js skills and
-              deepening that skill set as I build UI projects. I aim to grow as
-              this repository grows. I values feedback as I continue my coding
-              journey.
-            </p>
-
-            <img
-              src="/images/mySetup.jpeg"
-              alt="My Desktop Setup with Rubiks Cube and Cobra Duck"
-              className="max-w-lg p-8 rounded-2xl"
-            />
-            <p>
-              Being a Frontend Developer, I am drawn to the design process and
-              how the design is handed off to developers. There is so much tied
-              into UX/UI Design as a whole and what they do. From researching,
-              users behaviors, wire framing, collaboration, and designing a UI,
-              that not only looks appealing but has a good user experience. I
-              want to be the one turns the design into functional code.
-              Understanding both sides and be able to communicate with designers
-              and developers. With this repository, I also uncover accessibility
-              and practice methods throughout my projects. I hope as this
-              repository grows, I can develop my skills not only as a developer
-              but as a UI developer/designer. Focusing on user satisfaction,
-              usability, and accessibility. In essence, I am a UX Engineer.
-            </p>
-
-            <MyWork />
-          </section>
+      <main className="flex flex-col justify-center items-center bg-[url('/images/road.jpg')] bg-cover h-screen">
+        <h1 className="text-3xl md:text-6xl lg:text-7xl text-white font-ram">
+          Road To UXE
+        </h1>
+        <p className="mt-5 text-teal text-2xl font-serif">
+          Projects by: Davonne J. Vigil
+        </p>
+        <section className="flex flex-col justify-center items-center mt-20 mb-20 max-w-6xl lg:max-w-7xl p-8 bg-blue-200 bg-opacity-75 rounded-3xl">
+          <MyWork />
         </section>
       </main>
     </>
--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -4,7 +4,7 @@ export default function MyWork() {
   const myWork = [
     { id: 1, text: "St. Patrick's Day", link: "/st-patricks-day" },
     { id: 2, text: "Tobacco Shop", link: "/shop" },
-    { id: 3, text: "LinkedIn Mobile Login", link: "/next" },
+    { id: 3, text: "LinkedIn Mobile Login", link: "/linkedIn" },
     { id: 4, text: "240sx Nissan", link: "/blackPanther" },
     { id: 5, text: "Peace and Love", link: "/like" },
     { id: 6, text: "Storm Chasers", link: "/chasing" },
@@ -15,7 +15,7 @@ export default function MyWork() {
   ];
 
   return (
-    <nav className="grid grid-cols-1 md:grid-cols-2 mt-4 text-teal lg:grid-cols-3 lg:flex-row gap-10 lg:gap-8 font-sans text-2xl">
+    <nav className="grid grid-cols-1 md:grid-cols-2 mt-4 text-teal lg:grid-cols-3 lg:flex-row gap-10 lg:gap-8 font-serif text-2xl">
       {myWork.map((work, id) => (
         <li key={id} className="text-teal list-none">
           <Link to={work.link}>{work.text}</Link>
