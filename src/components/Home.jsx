a48cc0d4212a30d16852d7b4b09283e6655cf7ab
Vonney <davonnejv@gmail.com>
2024-05-21 17:43:17 -0500

    remove p element from Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,9 @@ export default function Home() {
           <h1 className="mt-10 md:mt-0 lg:mt-0 text-3xl md:text-6xl lg:text-7xl text-white font-ram">
             Road To UXE
           </h1>
-          <p className="mt-5 text-teal text-2xl font-serif">
-            Projects by: Davonne J. Vigil
-          </p>
+          {/* <p className="mt-5 text-teal text-2xl font-serif">
+            
+          </p> */}
           <section className="flex flex-col justify-center items-center mt-20 mb-20 max-w-6xl lg:max-w-7xl p-8 bg-blue-200 bg-opacity-75 rounded-3xl">
             <MyWork />
           </section>
