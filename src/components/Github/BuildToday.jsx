d40a457d8d8fd8e32ce515262a42c218292e17c0
Vonney <davonnejv@gmail.com>
2024-05-21 20:36:02 -0500

    add hero section

--- src/components/Github/BuildToday.jsx
+++ src/components/Github/BuildToday.jsx
@@ -1,10 +1,12 @@
 import CodeHeader from "./CodeHeader";
+import Together from "./Together";
 
 export default function BuildToday() {
   return (
     <>
       <main className="bg-git bg-cover min-h-screen">
         <CodeHeader />
+        <Together />
       </main>
     </>
   );
---
+++ src/components/Github/Together.jsx
@@ -0,0 +1,10 @@
+export default function Together() {
+  return (
+    <>
+      <section className="text-white flex gap-8 justify-center items-center mt-40 text-6xl font-sanss">
+        <p>Build Together,</p>
+        <p>Grow Together</p>
+      </section>
+    </>
+  );
+}
