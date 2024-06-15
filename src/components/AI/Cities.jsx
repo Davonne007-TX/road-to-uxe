acf07270e98e260c502905e6b8f1390f5dcca17c
Vonney <davonnejv@gmail.com>
2024-06-14 22:36:02 -0500

    make image its own component

---
+++ src/components/AI/Cities.jsx
@@ -0,0 +1,14 @@
+export default function Cities() {
+  return (
+    <>
+      <img
+        src="/images/cities.jpg"
+        alt="Blue Futuristic Cities, Image By benzoix on Freepik"
+        className="max-w-2xl"
+      />
+      <a href="https://www.freepik.com/free-photo/global-business-internet-network-connection-iot-internet-things-business-intelligence-concept-busines-global-network-futuristic-technology-background-ai-generative_49395794.htm#fromView=search&page=2&position=3&uuid=73271e7d-5a78-4236-8232-a4f4be73ae80">
+        Image by benzoix on Freepik
+      </a>
+    </>
+  );
+}
--- src/components/AI/Now.jsx
+++ src/components/AI/Now.jsx
@@ -1,19 +1,13 @@
 import AIButtons from "./AIButtons";
+import Cities from "./Cities";
 
 export default function Now({ title, text }) {
   return (
-    <section className="flex-time mt-20 md:mt-20 lg:mt-40 text-white">
-      <section className="max-w-xl md:max-w-2xl lg:max-w-3xl flex flex-col gap-10 bg-black bg-opacity-40 p-10 max-h-max">
+    <section className="flex-time mt-20 md:mt-24 lg:mt-24 text-white">
+      <section className="max-w-xl md:max-w-2xl lg:max-w-3xl mb-20 flex flex-col gap-10 bg-black bg-opacity-40 p-10 max-h-max">
         {" "}
         <h2 className="text-6xl font-ber">{title}</h2>
-        <img
-          src="/images/cities.jpg"
-          alt="Blue Futuristic Cities, Image By benzoix on Freepik"
-          className="max-w-2xl"
-        />
-        <a href="https://www.freepik.com/free-photo/global-business-internet-network-connection-iot-internet-things-business-intelligence-concept-busines-global-network-futuristic-technology-background-ai-generative_49395794.htm#fromView=search&page=2&position=3&uuid=73271e7d-5a78-4236-8232-a4f4be73ae80">
-          Image by benzoix on Freepik
-        </a>
+        <Cities />
         <p className=" text-2xl">{text}</p>
         <section className="flex gap-8">
           <AIButtons
