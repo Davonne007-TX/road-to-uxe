c5900b4389060be3029fbb5a781fa37bc280b49b
Vonney <davonnejv@gmail.com>
2024-06-17 16:47:47 -0500

    make header responsive for mobile devices for School Easter Project

--- src/components/Easter/EasterHeading.jsx
+++ src/components/Easter/EasterHeading.jsx
@@ -8,13 +8,13 @@ export default function EasterHeading() {
   return (
     <>
       <header className="bg-pink-200 p-2">
-        <section className="flex flex-row justify-between mt-4 font-serif">
-          <h1 className=" text-3xl lg:text-5xl ml-4 cursor-pointer">
+        <section className="flex flex-row justify-center items-center md:justify-between lg:justify-between mt-4 font-serif">
+          <h1 className="text-3xl lg:text-5xl ml-4 cursor-pointer">
             Lincoln Elementary
           </h1>
 
           <nav>
-            <ul className="flex flex-col md:flex-row lg:flex-row gap-10 mr-4 lg:ml-20 lg:mt-4 text-lg cursor-pointer">
+            <ul className=" hidden md:flex lg:flex flex-col md:flex-row lg:flex-row gap-10 mr-4 lg:ml-20 lg:mt-4 text-lg cursor-pointer">
               {navigationList.map((navigation, index) => (
                 <a href="" key={index}>
                   <li>{navigation.name}</li>
--- src/components/Easter/SecondHeading.jsx
+++ src/components/Easter/SecondHeading.jsx
@@ -2,7 +2,7 @@ export default function SecondHeading() {
   return (
     <>
       <section className="flex flex-col justify-center items-center bg-orange-100 p-2">
-        <p className="font-bold">
+        <p className="font-bold text-center">
           New After School Programs Now Available: STEM, Orchestra, CodeWiz
         </p>
       </section>
