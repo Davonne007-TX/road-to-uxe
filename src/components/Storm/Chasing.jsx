30176281d6c7c30aa3d911887ab682af90e950a8
Vonney <davonnejv@gmail.com>
2024-06-17 16:38:13 -0500

    re-do code for mobile responsiveness for section of Storm Chasing Project, add breakpoints

--- src/components/Storm/Chasing.jsx
+++ src/components/Storm/Chasing.jsx
@@ -4,9 +4,11 @@ export default function Chasing() {
   return (
     <>
       <main className="flex flex-col justify-center items-center bg-[url('./images/tornado.jpg')] bg-cover h-screen ">
-        <section className="flex flex-col justify-center items-center text-white max-w-5xl">
-          <h1 className="text-6xl lg:text-8xl font-serif ">Devin's Chasing</h1>
-          <p className="text-2xl lg:text-3xl mt-10 font-sans text-center">
+        <section className="flex flex-col justify-center items-center text-white max-w-xl md:max-w-3xl lg:max-w-4xl">
+          <h1 className="text-5xl md:text-6xl lg:text-7xl font-serif text-center ">
+            Devin's Chasing
+          </h1>
+          <p className="text-xl lg:text-2xl mt-4 font-sans text-center">
             Nunc pulvinar sapien et ligula ullamcorper malesuada. Iaculis at
             erat pellentesque adipiscing commodo elit at imperdiet. Eu
             scelerisque felis imperdiet proin. Eget dolor morbi non arcu risus
@@ -14,7 +16,7 @@ export default function Chasing() {
           </p>
           <Button
             label="Stream Live"
-            className="bg-white text-black p-2 rounded-full hover:scale-110 text-xl lg:text-2xl mt-4 font-serif"
+            className="bg-white text-black p-2 rounded-full hover:scale-110 text-lg mt-4 font-serif"
           />
         </section>
       </main>
