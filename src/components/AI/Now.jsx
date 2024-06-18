354c88ef61a95c3a9ceda10b29582b91a8f44868
Vonney <davonnejv@gmail.com>
2024-06-17 19:44:27 -0500

    update margin top on hero section

--- src/components/AI/Now.jsx
+++ src/components/AI/Now.jsx
@@ -4,7 +4,7 @@ import Cities from "./Cities";
 export default function Now({ title, text }) {
   return (
     <main className="flex-time mt-20 md:mt-20 lg:mt-10 text-white">
-      <section className="max-w-sm md:max-w-2xl lg:max-w-3xl mb-20 flex flex-col gap-10 bg-black bg-opacity-40 p-10 max-h-max ">
+      <section className="max-w-sm md:max-w-2xl lg:max-w-3xl mt-10 md:mt-0 lg:mt-0 mb-20 flex flex-col gap-10 bg-black bg-opacity-40 p-10 max-h-max ">
         {" "}
         <h2 className="text-3xl md:text-5xl lg:text-6xl text-center md:mr-auto lg:mr-auto font-ber">
           {title}
