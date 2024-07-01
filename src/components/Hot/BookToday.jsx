ecb965a5f8f83bbe55a1b99d766cf77b2f60a9ac
Vonney <davonnejv@gmail.com>
2024-06-30 21:00:57 -0500

    add @apply to paragraph on hero section

--- src/components/Hot/BookToday.jsx
+++ src/components/Hot/BookToday.jsx
@@ -7,12 +7,12 @@ export default function BookToday() {
         <h2 className=" text-white font-bold text-3xl md:text-5xl lg:text-5xl">
           Your Vacation Starts <span className="text-blue-600">Today</span>
         </h2>
-        <p className="text-white ml-2 text-2xl mt-4">
+        <p className="vacation-styles">
           Book today. Feugiat vivamus at augue eget arcu dictum varius duis at.
           Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend
           donec pretium.
         </p>
-        <p className="text-white ml-2 text-2xl mt-4">
+        <p className="vacation-styles">
           Sapien pellentesque habitant morbi tristique senectus et netus et.
           Habitant morbi tristique senectus et netus et malesuada fames ac. Odio
           facilisis mauris sit amet.
