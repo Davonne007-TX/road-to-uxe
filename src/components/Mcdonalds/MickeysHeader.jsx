4a30be73c60c576354cc8b1a9305cd7d43cebed3
Vonney <davonnejv@gmail.com>
2024-07-09 01:52:24 -0500

    add nav bar and mobile nav bar

--- src/components/Mcdonalds/MickeysHeader.jsx
+++ src/components/Mcdonalds/MickeysHeader.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 export default function MickeysHeader() {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  const toggleNavBar = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
   const micksList = [
     { navLink: "Menu", id: 0 },
     { navLink: "Mickeys App", id: 1 },
@@ -11,10 +19,23 @@ export default function MickeysHeader() {
 
   return (
     <>
-      <header className="flex flex-row justify-center items-center gap-10 mt-10 font-thin cursor-pointer">
-        <img src="/images/mickeyLogo.jpg" className="w-48 h-auto ml-10" />
-        <section className="flex flex-col">
-          <section className="flex gap-10 list-none">
+      <header className="flex justify-between items-center p-4 bg-gray-100 relative z-10">
+        <img
+          src="/images/mickeyLogo.jpg"
+          className="w-32 h-auto"
+          alt="Mickey's Logo"
+        />
+        <button
+          className={`lg:hidden text-3xl hover:text-red-700 z-50 ${
+            isMobileNavOpen ? "fixed top-4 right-4" : "absolute top-4 right-4"
+          }`}
+          onClick={toggleNavBar}
+        >
+          ☰
+        </button>
+
+        <section className="flex-col mt-4">
+          <section className=" hidden lg:flex gap-10 list-none">
             <select className="rounded-2xl">
               <option selected>Languages</option>
               <option>English</option>
@@ -28,19 +49,23 @@ export default function MickeysHeader() {
             <li>Sign Up For Emails</li>
             <li>Start Your Career</li>
           </section>
-
-          <ul className="flex gap-10 mt-6">
+          <ul
+            className={`lg:flex gap-8 lg:gap-10 mt-4 lg:mt-0 ${
+              isMobileNavOpen
+                ? "fixed inset-0 flex flex-col justify-center items-center bg-gray-100 z-40"
+                : "hidden"
+            } lg:flex`}
+          >
             {micksList.map((micksNav, id) => (
               <li
                 key={id}
-                className="hover:font-bold hover:border-b-2 hover:border-red-700"
+                className="hover:font-bold hover:border-b-2 hover:border-red-700 text-xl p-4"
               >
                 {micksNav.navLink}
               </li>
             ))}
           </ul>
         </section>
-        <hr />
       </header>
     </>
   );
