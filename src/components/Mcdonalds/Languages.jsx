f8d797305afd124c6a29625ce84bc0a2602ae597
Vonney <davonnejv@gmail.com>
2024-07-09 12:58:35 -0500

    header complete

--- src/components/Mcdonalds/Languages.jsx
+++ src/components/Mcdonalds/Languages.jsx
@@ -2,7 +2,7 @@ export default function Languages() {
   return (
     <section className=" hidden md:mr-80 lg:mr-48 mt-4 lg:flex lg:justify-center lg:items-center gap-10  list-none cursor-pointer">
       <select className="rounded-xl cursor-pointer text-sm w-31 h-10">
-        <option selected>Languages</option>
+        <option value="Languages">Languages</option>
         <option>English</option>
         <option>Spanish</option>
         <option>French</option>
