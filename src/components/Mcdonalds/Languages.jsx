a797243764f907cdfd40e8e6d42686b6c1868f50
Vonney <davonnejv@gmail.com>
2024-07-09 01:57:00 -0500

    make component for drop down for maintainable code

---
+++ src/components/Mcdonalds/Languages.jsx
@@ -0,0 +1,18 @@
+export default function Languages() {
+  return (
+    <section className=" hidden lg:flex gap-10 list-none">
+      <select className="rounded-2xl">
+        <option selected>Languages</option>
+        <option>English</option>
+        <option>Spanish</option>
+        <option>French</option>
+        <option>Arabic</option>
+        <option>Russian</option>
+        <option>Mandarin Chinese</option>
+        <option>Japanese</option>
+      </select>
+      <li>Sign Up For Emails</li>
+      <li>Start Your Career</li>
+    </section>
+  );
+}
--- src/components/Mcdonalds/MickeysHeader.jsx
+++ src/components/Mcdonalds/MickeysHeader.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Languages from "./Languages";
 
 export default function MickeysHeader() {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
@@ -35,20 +36,8 @@ export default function MickeysHeader() {
         </button>
 
         <section className="flex-col mt-4">
-          <section className=" hidden lg:flex gap-10 list-none">
-            <select className="rounded-2xl">
-              <option selected>Languages</option>
-              <option>English</option>
-              <option>Spanish</option>
-              <option>French</option>
-              <option>Arabic</option>
-              <option>Russian</option>
-              <option>Mandarin Chinese</option>
-              <option>Japanese</option>
-            </select>
-            <li>Sign Up For Emails</li>
-            <li>Start Your Career</li>
-          </section>
+          <Languages />
+
           <ul
             className={`lg:flex gap-8 lg:gap-10 mt-4 lg:mt-0 ${
               isMobileNavOpen
