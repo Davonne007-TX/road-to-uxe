f1c473553c9e507e51a0b5ff2f91d977affa5c80
Davonne <davonnejv@gmail.com>
2024-09-24 10:41:20 -0500

    feature more sections about what the school offers

--- src/components/Easter/Bunny.jsx
+++ src/components/Easter/Bunny.jsx
@@ -1,5 +1,4 @@
 import EasterHeading from "./EasterHeading";
-import SecondHeading from "./SecondHeading";
 import Hero from "./Hero";
 import About from "./About";
 import Easter from "./Easter";
@@ -16,14 +15,21 @@ export default function Bunny() {
           <Easter
             title="Easter Egg Hunt Friday March 29th"
             src="/images/hunt.jpg"
-            alt="Children with Eater Egg Baskets and Easter Eggs, Photo by Gabe Pierce on Unsplash"
+            alt="Children with Eater Egg Baskets and Easter Eggs, Photo from unsplash: by Gabe Pierce on Unsplash"
             text="Consectetur purus ut faucibus pulvinar elementum integer enim. Pellentesque dignissim enim sit amet venenatis urna cursus. Fermentum et sollicitudin ac orci phasellus. Sollicitudin tempor id eu nisl. Aenean et tortor at risus viverra adipiscing."
             moreText="At tempor commodo ullamcorper a lacus vestibulum. At augue eget arcu dictum varius duis at. Sollicitudin tempor id eu nisl. Aenean et tortor at risus viverra adipiscing. Vivamus at augue eget arcu dictum varius duis at consectetur."
           />
           <Easter
             title="CodeWiz, An After School Program To Learn To Code"
             src="/images/kidsCode.jpg"
-            alt="Children Coding on Desktop Computers, Photo By Gustavo Fring"
+            alt="Children Coding on Desktop Computers, Photo From unsplash: by Gustavo Fring"
+            text="Consectetur purus ut faucibus pulvinar elementum integer enim. Pellentesque dignissim enim sit amet venenatis urna cursus. Fermentum et sollicitudin ac orci phasellus. Sem et tortor consequat id."
+            moreText="Sollicitudin tempor id eu nisl. Aenean et tortor at risus viverra adipiscing. Vivamus at augue eget arcu dictum varius duis at consectetur. Sit amet est placerat in egestas erat imperdiet sed euismod."
+          />
+          <Easter
+            title="Dedicated Teachers Hard Working Teachers"
+            src="/images/teachers.jpg"
+            alt="Male Teachers working at his desk in his classroom, Photo from unsplash: by Gustavo Jeswin Thomas"
             text="Consectetur purus ut faucibus pulvinar elementum integer enim. Pellentesque dignissim enim sit amet venenatis urna cursus. Fermentum et sollicitudin ac orci phasellus. Sem et tortor consequat id."
             moreText="Sollicitudin tempor id eu nisl. Aenean et tortor at risus viverra adipiscing. Vivamus at augue eget arcu dictum varius duis at consectetur. Sit amet est placerat in egestas erat imperdiet sed euismod."
           />
@@ -32,9 +38,3 @@ export default function Bunny() {
     </>
   );
 }
-
-//First Image
-// Photo by <a href="https://unsplash.com/@gaberce?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Gabe Pierce</a> on <a href="https://unsplash.com/photos/girl-in-green-jacket-playing-with-white-and-pink-plastic-balls-on-green-grass-field-during-2w46Lv6EYT4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-
-//Second Image
-// Photo by Gustavo Fring: <a href="https://www.pexels.com/photo/kids-in-a-computer-lab-5621945/">https://www.pexels.com/photo/kids-in-a-computer-lab-5621945/"</a>
