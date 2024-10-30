755313398547269522226146d927e1323117e23b
Davonne <davonnejv@gmail.com>
2024-10-29 20:37:24 -0500

    add styles for footer using flex and grid

--- src/components/Mcdonalds/Footer.jsx
+++ src/components/Mcdonalds/Footer.jsx
@@ -3,17 +3,24 @@ import footerSections from "./data/footerLinks.js";
 export default function Footer() {
   return (
     <>
-      <section className="mt-20">
-        {footerSections.map((section, index) => (
-          <section key={index}>
-            <h3>{section.title}</h3>
-            <ul>
-              {section.links.map((link, linkIndex) => (
-                <li key={linkIndex}>{link}</li>
-              ))}
-            </ul>
-          </section>
-        ))}
+      <section className="mt-20 m-0 bg-black p-4">
+        <section className="max-w-6xl ml-auto mr-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+          {footerSections.map((section, index) => (
+            <section key={index}>
+              <h3 className="font-bold text-yellow-400 ">{section.title}</h3>
+
+              <ul className=" gap-2 text-white">
+                {section.links.map((link, linkIndex) => (
+                  <>
+                    <a href="" className="hover:underline">
+                      <li key={linkIndex}>{link}</li>
+                    </a>
+                  </>
+                ))}
+              </ul>
+            </section>
+          ))}
+        </section>
       </section>
     </>
   );
