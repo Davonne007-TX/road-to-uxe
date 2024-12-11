2015692149e035ac6e241e3bf08ec8bac1084c11
Davonne Vigil <davonnejv@gmail.com>
2024-12-10 19:34:10 -0600

    add image inside of reuseable component Now.jsx so we can change each image per article

--- src/components/AI/Future.jsx
+++ src/components/AI/Future.jsx
@@ -8,7 +8,21 @@ export default function Future() {
         <FutureHeader />
         <Now
           title="The Future is Here"
-          text="Gravida in fermentum et sollicitudin ac orci phasellus. Convallis aenean et tortor at risus viverra. Nam aliquam sem et tortor consequat id porta nibh venenatis."
+          image="/images/ai.jpg"
+          alt="AI Robots Heads, Photo by julien Tromeur on Unsplash"
+          text="Gravida in fermentum et sollicitudin ac orci phasellus. Convallis aenean et tortor at risus viverra. Nam aliquam sem et tortor consequat id porta nibh venenatis. Gravida in fermentum et sollicitudin ac orci phasellus. Convallis aenean et tortor at risus viverra."
+        />
+        <Now
+          title="The Future is Here"
+          image="/images/ai.jpg"
+          alt="AI Robots Heads, Photo by julien Tromeur on Unsplash"
+          text="Gravida in fermentum et sollicitudin ac orci phasellus. Convallis aenean et tortor at risus viverra. Nam aliquam sem et tortor consequat id porta nibh venenatis.Gravida in fermentum et sollicitudin ac orci phasellus. Convallis aenean et tortor at risus viverra."
+        />
+        <Now
+          title="The Future is Here"
+          image="/images/ai.jpg"
+          alt="AI Robots Heads, Photo by julien Tromeur on Unsplash"
+          text="Gravida in fermentum et sollicitudin ac orci phasellus. Convallis aenean et tortor at risus viverra. Nam aliquam sem et tortor consequat id porta nibh venenatis. Gravida in fermentum et sollicitudin ac orci phasellus. Convallis aenean et tortor at risus viverra."
         />
       </main>
     </>
--- src/components/AI/Now.jsx
+++ src/components/AI/Now.jsx
@@ -1,19 +1,18 @@
 import AIButtons from "./AIButtons";
-import Cities from "./Cities";
 
-export default function Now({ title, text }) {
+export default function Now({ title, text, image, alt }) {
   return (
-    <main className="flex-time mt-20 md:mt-20 lg:mt-10 text-white">
-      <section className="max-w-sm md:max-w-2xl lg:max-w-3xl mt-10 md:mt-0 lg:mt-0 mb-20 flex flex-col gap-10 bg-black bg-opacity-40 p-10 max-h-max ">
+    <main className="flex-time mt-20 md:mt-20 text-white">
+      <section className="max-w-sm md:max-w-2xl lg:max-w-3xl mt-10 md:mt-0 mb-10 flex flex-col gap-8 bg-black bg-opacity-40 p-10 max-h-max ">
         {" "}
         <h2 className="text-3xl md:text-5xl lg:text-6xl text-center md:mr-auto lg:mr-auto font-ber">
           {title}
         </h2>
-        <Cities />
-        <p className="text-lg md:text-2xl lg:text-2xl -mt-4 ">{text}</p>
+        <img src={image} alt={alt} />
+        <p className="text-lg md:text-2xl lg:text-2xl -mt-4">{text}</p>
         <section className="flex gap-8">
-          <AIButtons text="Learn More" styles="ai-buttons" />
-          <AIButtons text="AI Blog" styles="ai-buttons" />
+          <AIButtons text="Learn more" styles="ai-buttons" />
+          <AIButtons text="Related articles" styles="ai-buttons" />
         </section>
       </section>
     </main>
