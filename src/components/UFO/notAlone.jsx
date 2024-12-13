425c26b4b6a2eb2bafe0d2466d5471c5e44bf927
Davonne Vigil <davonnejv@gmail.com>
2024-12-12 21:01:19 -0600

    add background image

--- src/components/UFO/notAlone.jsx
+++ src/components/UFO/notAlone.jsx
@@ -1,7 +1,11 @@
 export default function NotAlone() {
   return (
-    <main>
-      <h1>Are They Already Here</h1>
+    <main className="bg-[url('/images/ufo.png')] bg-cover min-h-screen">
+      <section className="flex-time">
+        <h1 className="text-white mt-96 lg:p-40 font-thin text-5xl">
+          Coming to a town near you
+        </h1>
+      </section>
     </main>
   );
 }
