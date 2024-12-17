b4aa4c61d9347a0b91402347f0455570d227a90b
Davonne Vigil <davonnejv@gmail.com>
2024-12-17 10:12:34 -0600

    move hero section

---
+++ src/components/UFO/Unknown.jsx
@@ -0,0 +1,18 @@
+export default function Unknown() {
+  return (
+    <section className="ml-20 mt-72 flex flex-col  p-8 rounded">
+      <section className="max-w-3xl p-8 bg-black bg-opacity-50 flex flex-col gap-4">
+        <h2 className="text-white font-thin text-5xl rounded">
+          Coming to a town near you
+        </h2>
+        <p className="text-2xl">
+          {" "}
+          Call them drones, call them ufos, uaps. Either way, there is something
+          in the sky. Seems like ever since 2020, anything is possible. Our site
+          lets you explore recent ufo sighting, recent reports, documents, and
+          the history of the unknown in our skies.
+        </p>
+      </section>
+    </section>
+  );
+}
--- src/components/UFO/notAlone.jsx
+++ src/components/UFO/notAlone.jsx
@@ -1,14 +1,11 @@
 import Nav from "./Nav";
+import Unknown from "./Unknown";
 
 export default function NotAlone() {
   return (
     <main className="bg-[url('/images/ufo.png')] bg-cover min-h-screen text-white">
-      <section>
-        <Nav />
-        <h2 className="text-white flex justify-center items-center mt-80 p-8 font-thin text-5xl">
-          Coming to a town near you
-        </h2>
-      </section>
+      <Nav />
+      <Unknown />
     </main>
   );
 }
