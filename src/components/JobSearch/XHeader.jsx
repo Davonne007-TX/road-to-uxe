05a6f9994c6a1a83e4fddeb0738419258bc6eca7
Davonne Vigil <davonnejv@gmail.com>
2024-12-31 15:42:19 -0600

    add nav bar

--- src/components/JobSearch/XHeader.jsx
+++ src/components/JobSearch/XHeader.jsx
@@ -1,14 +1,25 @@
 export default function XHeader() {
   const listItems = [
-    { navLink: "X", id: 1 },
-    { navLink: "Job Search", id: 2 },
-    { navLink: "Referrals", id: 3 },
+    { navLink: "Job Search", id: 1 },
+    { navLink: "Referrals", id: 2 },
+    { navLink: "Submitted Applications", id: 3 },
+    { navLink: "For Businesses", emphasis: "New", id: 4 },
   ];
 
   return (
-    <nav className="text-white flex gap-20 font-bold p-4 list-none">
+    <nav className="text-white text-4xl flex gap-20 font-bold p-4 list-none">
+      X
       {listItems.map((item) => (
-        <li key={item.id}>{item.navLink}</li>
+        <>
+          <li key={item.id} className="text-lg mt-2">
+            {item.navLink}
+            {item.emphasis && (
+              <span className="text-white ml-4 bg-purple-600 p-2 rounded-xl">
+                {item.emphasis}
+              </span>
+            )}
+          </li>
+        </>
       ))}
     </nav>
   );
