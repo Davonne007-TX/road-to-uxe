1bbde1aaae6d1b0fa09a9599d26047664ae2c17b
Davonne Vigil <davonnejv@gmail.com>
2024-12-31 11:45:38 -0600

    add route and link to X Job Seach Clone

---
+++ src/components/JobSearch/XHeader.jsx
@@ -0,0 +1,3 @@
+export default function XHeader() {
+  return <div className="text-white">XHeader</div>;
+}
---
+++ src/components/JobSearch/XJobSearch.jsx
@@ -0,0 +1,10 @@
+import XHeader from "./XHeader";
+
+export default function XJobSearch() {
+  return (
+    <section>
+      <h1>X Job Search</h1>
+      <XHeader />
+    </section>
+  );
+}
