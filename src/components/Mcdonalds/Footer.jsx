71bbed0049914b5fb186cc33bfb66f77de0b7876
Davonne Vigil <davonnejv@gmail.com>
2025-01-03 21:34:54 -0600

    key error in Mcdonalds footer

--- src/components/Mcdonalds/Footer.jsx
+++ src/components/Mcdonalds/Footer.jsx
@@ -2,26 +2,23 @@ import footerSections from "./data/footerLinks.js";
 
 export default function Footer() {
   return (
-    <>
-      <section className="mt-20 m-0 bg-black p-4">
-        <section className="max-w-5xl ml-auto mr-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-          {footerSections.map((sections) => (
-            <section key={sections.id} className="flex flex-col gap-2">
-              <h3 className="font-bold text-yellow-400">{sections.title}</h3>
-
-              <ul className="text-white">
-                {sections.links.map((link) => (
-                  <>
-                    <a href="#" key={link.id} className="hover:underline">
-                      <li>{link}</li>
-                    </a>
-                  </>
-                ))}
-              </ul>
-            </section>
-          ))}
-        </section>
+    <section className="mt-20 m-0 bg-black p-4">
+      <section className="max-w-5xl ml-auto mr-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        {footerSections.map((section) => (
+          <section key={section.id} className="flex flex-col gap-2">
+            <h3 className="font-bold text-yellow-400">{section.title}</h3>
+            <ul>
+              {section.links.map((link, index) => (
+                <li key={link.id || index} className="text-white">
+                  <a href="#" className="hover:underline">
+                    {link.name || link}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </section>
+        ))}
       </section>
-    </>
+    </section>
   );
 }
