f283c6a0d1a2cc05072ffe8b9d1b9c55b6fedc21
Davonne Vigil <davonnejv@gmail.com>
2025-03-17 10:35:02 -0500

    update: header on Code Together project, make everything aligned

--- src/components/Github/Input.jsx
+++ src/components/Github/Input.jsx
@@ -3,13 +3,13 @@ export default function Input() {
     <section className="relative mt-4 mb-4 font-serif">
       <input
         placeholder="Search For Repository"
-        className="w-72 md:w-96 lg:w-80 h-auto p-2 bg-gray-300 rounded-2xl text-black outline-none"
+        className="w-72 md:w-96 lg:w-80 md:mt-4 h-auto p-2 bg-gray-300 rounded-2xl text-black outline-none"
       />
       <button>
         <img
           src="/images/magnifying-glass.png"
           alt="Global Search, Magnifying Glass by Icons8"
-          className="w-8 h-auto absolute right-4 top-1/2 transform -translate-y-1/2"
+          className="w-8 h-auto absolute right-4 top-1/2 transform -translate-y-1/2 md:mt-2"
         />
       </button>
     </section>
