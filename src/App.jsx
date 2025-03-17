ca77d8b5a471544dd0382d351c9f40a81e791958
Davonne Vigil <davonnejv@gmail.com>
2025-03-17 13:34:49 -0500

    feature: suspense, and react lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,57 @@
+import React, { Suspense } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Shop from "./components/Shop";
-import Paddys from "./components/Paddys";
-import LinkedIn from "./components/LinkedIn/LinkedIn";
-import Search from "./components/Journey/Search";
-import Next from "./components/LinkedIn/Next";
-import BlackPanther from "./components/240/BlackPanther";
-import Like from "./components/Peace/Like";
-import Chasing from "./components/Storm/Chasing";
-import Shrimp from "./components/Seafood/Shrimp";
-import Bunny from "./components/Easter/Bunny";
-import Cola from "./components/Soda/Cola";
-import BuildToday from "./components/Github/BuildToday";
-import Future from "./components/AI/Future";
-import Summer from "./components/Hot/Summer";
-import MickeyDees from "./components/Mcdonalds/MickeyDees";
-import NotAlone from "./components/UFO/NotAlone";
-import XJobSearch from "./components/JobSearch/XJobSearch";
+
+const Home = React.lazy(() => import('./components/Home'))
+const Shop = React.lazy(() => import('./components/Shop'))
+const Paddys = React.lazy(() => import('./components/Paddys'))
+const LinkedIn = React.lazy(() => import('./components/LinkedIn/LinkedIn'))
+const Search = React.lazy(() => import('./components/Journey/Search'))
+const Next = React.lazy(() => import("./components/LinkedIn/Next"))
+const BlackPanther = React.lazy(() => import('./components/240/BlackPanther'))
+const Like = React.lazy(() => import('./components/Peace/Like'))
+const Chasing = React.lazy(() => import('./components/Storm/Chasing'))
+const Shrimp = React.lazy(() => import('./components/Seafood/Shrimp'))
+const Bunny = React.lazy(() => import('./components/Easter/Bunny'))
+const Cola = React.lazy(() => import('./components/Soda/Cola'))
+const BuildToday = React.lazy(() => import('./components/Github/BuildToday'))
+const Future = React.lazy(() => import('./components/AI/Future'))
+const Summer = React.lazy(() => import('./components/Hot/Summer'))
+const MickeyDees = React.lazy(() => import('./components/Mcdonalds/MickeyDees'))
+const NotAlone = React.lazy(() => import('./components/UFO/NotAlone'))
+const XJobSearch = React.lazy(() => import('./components/JobSearch/XJobSearch'))
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/st-patricks-day" element={<Paddys />} />
-        <Route path="/linkedIn" element={<LinkedIn />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/next" element={<Next />} />
-        <Route path="/blackPanther" element={<BlackPanther />} />
-        <Route path="/like" element={<Like />} />
-        <Route path="/chasing" element={<Chasing />} />
-        <Route path="/seafood" element={<Shrimp />} />
-        <Route path="/easter" element={<Bunny />} />
-        <Route path="/cola" element={<Cola />} />
-        <Route path="/together" element={<BuildToday />} />
-        <Route path="/future" element={<Future />} />
-        <Route path="/summer" element={<Summer />} />
-        <Route path="/mickeyDees/" element={<MickeyDees />} />
-        <Route path="/ufo" element={<NotAlone />} />
-        <Route path="/xJobSearch" element={<XJobSearch />} />
-      </Routes>
+      <Suspense fallback={
+        <section className="bg-black text-blue-300 font-caption text-center flex flex-time min-h-screen">
+          <p className="text-3xl md:text-7xl  text-blue-500">Loading UI Projects</p>
+        </section>
+      }>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/st-patricks-day" element={<Paddys />} />
+          <Route path="/linkedIn" element={<LinkedIn />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/next" element={<Next />} />
+          <Route path="/blackPanther" element={<BlackPanther />} />
+          <Route path="/like" element={<Like />} />
+          <Route path="/chasing" element={<Chasing />} />
+          <Route path="/seafood" element={<Shrimp />} />
+          <Route path="/easter" element={<Bunny />} />
+          <Route path="/cola" element={<Cola />} />
+          <Route path="/together" element={<BuildToday />} />
+          <Route path="/future" element={<Future />} />
+          <Route path="/summer" element={<Summer />} />
+          <Route path="/mickeyDees/" element={<MickeyDees />} />
+          <Route path="/ufo" element={<NotAlone />} />
+          <Route path="/xJobSearch" element={<XJobSearch />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
 
 export default App;
-
-//need to do react lazy
