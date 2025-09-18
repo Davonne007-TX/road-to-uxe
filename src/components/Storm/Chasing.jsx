e932b0cbc45c5726512164a6f2483c95f248619d
Davonne Vigil <davonnejv@gmail.com>
2025-09-17 23:52:15 -0500

    update: text in Devins Chasing, update section responsiveness

--- src/components/Storm/Chasing.jsx
+++ src/components/Storm/Chasing.jsx
@@ -4,19 +4,20 @@ export default function Chasing() {
   return (
     <>
       <main className="flex flex-col justify-center items-center bg-[url('/images/tornado.jpg')] bg-cover h-screen ">
-        <section className="flex flex-col justify-center items-center text-white max-w-xl md:max-w-3xl lg:max-w-4xl">
+        <section className="flex flex-col justify-center items-center bg-black/10 p-4 rounded text-white max-w-2xl">
           <h1 className="text-5xl md:text-6xl lg:text-7xl font-serif text-center ">
             Devin's Chasing
           </h1>
           <p className="text-xl lg:text-2xl mt-4 font-sans text-center">
-            Nunc pulvinar sapien et ligula ullamcorper malesuada. Iaculis at
-            erat pellentesque adipiscing commodo elit at imperdiet. Eu
-            scelerisque felis imperdiet proin. Eget dolor morbi non arcu risus
-            quis varius quam. At ultrices mi tempus imperdiet nulla.
+            Get Your Cameras, get your GoPro. It is Tornado Chasing Season! Its
+            time to grab our gear and chase storms. With our knowledge of
+            storms, and weather tracking, we are able to be in the right spot
+            when it comes to tornadoes. We aim to collect data in order to give
+            it local officials and share to our social media pages.
           </p>
           <Button
-            label="Stream Live"
-            className="bg-white text-black p-2 rounded-full hover:scale-110 text-lg mt-4 font-serif"
+            label="Live Stream"
+            className="bg-white  text-black p-2 rounded-full hover:scale-110 text-lg mt-4 font-serif"
           />
         </section>
       </main>
